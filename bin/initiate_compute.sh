ee89b9e98efe92c96c13a60e29ba3e42ee5993ce
Jacob Luber <jluber@g.harvard.edu>
2017-07-06 12:50:19 -0400

    Update initiate_compute.sh

--- bin/initiate_compute.sh
+++ bin/initiate_compute.sh
@@ -88,5 +88,5 @@ check_connection()
 check_connection
 
 chmod 400 "$5".pem
-ssh -i "$5".pem -o StrictHostKeyChecking=no ubuntu@$IP 'sudo apt-get update; sudo apt-get install python awscli jq --assume-yes; printf "'${6}'\n'${7}'\n'${8}'\n\n" | /usr/bin/aws configure;/usr/bin/aws s3 cp s3://'$5'/tempkey/'$5'.pem .;sudo chmod 400 '$5'.pem; git clone git@github.com:kosticlab/aether.git;cd aether;cd bin;sudo chmod +x *.sh;cd ..;./bin/primary_node_init.sh '$1' '$2' '$3' '$4' '$5' '$6' '$7' '$9' '${10}' 2> error.log &'
+ssh -i "$5".pem -o StrictHostKeyChecking=no ubuntu@$IP 'sudo apt-get update; sudo apt-get install python awscli jq --assume-yes; printf "'${6}'\n'${7}'\n'${8}'\n\n" | /usr/bin/aws configure;/usr/bin/aws s3 cp s3://'$5'/tempkey/'$5'.pem .;sudo chmod 400 '$5'.pem; git clone https://github.com/kosticlab/aether.git;cd aether;cd bin;sudo chmod +x *.sh;cd ..;./bin/primary_node_init.sh '$1' '$2' '$3' '$4' '$5' '$6' '$7' '$9' '${10}' 2> error.log &'
 
